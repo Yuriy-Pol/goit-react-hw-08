3af39ef3e18d8486f706e8a1c7c543cccd1e6349
Yurii <141032913+Yuriy-Pol@users.noreply.github.com>
2024-04-03 22:11:10 +0300

    Initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,40 @@
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+
+import authReducer from './auth/slice.js';
+import contactsReducer from './contacts/slice.js';
+import filtersReducer from './filters/slice.js';
+
+const authPersistConfig = {
+  key: 'authSlice',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
